447c5c587ca2584bfa3a7aa6fe4e65e0d85048c0
lssantos <ls.corporativo18@gmail.com>
2024-10-02 19:39:58 -0300

    Lista/Produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Aether Gym Pant')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+
+});
