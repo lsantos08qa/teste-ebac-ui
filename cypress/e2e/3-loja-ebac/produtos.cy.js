ee98f45c277f1aa63c506f95ca3ec6e0c8a59afc
lssantos <ls.corporativo18@gmail.com>
2024-10-04 16:43:42 -0300

    Produtos Page

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,29 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-    cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Aether Gym Pant')
-            .click()
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')        
+    });
 
-            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a pagina do produto', () => {
+        
+    });
+
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
