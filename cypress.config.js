4964699e988909610aa60b50dd27a7b9403141ea
lssantos <ls.corporativo18@gmail.com>
2024-10-07 10:46:39 -0300

    Mudança no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '38428h',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://lojaebac.ebaconline.art.br/'
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
   },
 });
---
+++ cypress/e2e/loja-ebac/produtos.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        produtosPage.visitarUrl()
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')        
+    });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a pagina do produto', () => {
+        produtosPage.visitarProduto('Ajax Full-Zip Sweatshirt')
+        cy.get('.product_title').should('contain', 'Ajax Full-Zip Sweatshirt')
+    });
+
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('L', 'Blue', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[2].tamanho,
+                dados[2].cor, 
+                dados[2].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+        })       
+    });
+});
